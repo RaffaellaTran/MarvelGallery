81d2cbb3c574975bc73bd82011a111a2e3cd3493
Raffaella <raffaella.tran@dunnhumby.com>
2018-11-02 16:21:20 +0100

    Show list from API
    Connecting the MainPresenter with the MainActivity to show the list of Marvel characters from the Marvel API.

--- app/src/main/java/com/example/rafaellat/marvelgallery/view/common/ViewExt.kt
+++ app/src/main/java/com/example/rafaellat/marvelgallery/view/common/ViewExt.kt
@@ -1,11 +1,44 @@
 package com.example.rafaellat.marvelgallery.view.common
 
+import android.app.Activity
+import android.content.Context
+import android.support.annotation.IdRes
+import android.support.v4.widget.SwipeRefreshLayout
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
+//fun <T : View> RecyclerView.ViewHolder.bindView(viewId: Int) = lazy { itemView.findViewById<T>(viewId) }
 fun ImageView.loadImage(photoUrl: String, centerCropped: Boolean = false) {
     Glide.with(context)
         .load(photoUrl)
         .apply { if (centerCropped) centerCrop() }
         .into(this)
+}
+
+fun Context.toast(text: String, length: Int = Toast.LENGTH_LONG) {
+    Toast.makeText(this, text, length).show()
+}
+
+fun Activity.bindToSwipeRefresh(@IdRes swipeRefreshLayoutId: Int): ReadWriteProperty<Any?, Boolean> =
+    SwipeRefreshBinding(lazy { findViewById<SwipeRefreshLayout>(swipeRefreshLayoutId) })
+
+private class SwipeRefreshBinding(lazyViewProvider: Lazy<SwipeRefreshLayout>) : ReadWriteProperty<Any?, Boolean> {
+    val view by lazyViewProvider
+
+    override fun getValue(
+        thisRef: Any?,
+        property: KProperty<*>
+    ): Boolean {
+        return view.isRefreshing
+    }
+
+    override fun setValue(
+        thisRef: Any?,
+        property: KProperty<*>, value: Boolean
+    ) {
+        view.isRefreshing = value
+    }
 }
--- app/src/main/java/com/example/rafaellat/marvelgallery/view/main/MainActivity.kt
+++ app/src/main/java/com/example/rafaellat/marvelgallery/view/main/MainActivity.kt
@@ -1,31 +1,39 @@
 package com.example.rafaellat.marvelgallery.view.main
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.view.Window
 import com.example.rafaellat.marvelgallery.R
+import com.example.rafaellat.marvelgallery.data.MarvelRepository
 import com.example.rafaellat.marvelgallery.model.MarvelCharacter
+import com.example.rafaellat.marvelgallery.presenter.MainPresenter
+import com.example.rafaellat.marvelgallery.view.common.BaseActivityWithPresenter
+import com.example.rafaellat.marvelgallery.view.common.bindToSwipeRefresh
+import com.example.rafaellat.marvelgallery.view.common.toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivityWithPresenter(),
+    MainView { // activity should extend BaseActivityWithPresenter and implement MainView
 
-    private val characters = listOf(
-        MarvelCharacter(name = "3-D Man", imageUrl = "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
-        MarvelCharacter(
-            name = "Abomination (Emil Blonsky)",
-            imageUrl = "http://i.annihil.us/u/prod/marvel/i/mg/9/50/4ce18691cbf04.jpg"
-        )
-    )
+    override var refresh by bindToSwipeRefresh(R.id.swipeRefreshView) // function for bind a property with the visibility of the swipe refresh
+    override val presenter by lazy { MainPresenter(this, MarvelRepository.get()) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE) // no title
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
         recyclerView.layoutManager = GridLayoutManager(this, 2)
-        val categoryItemAdapters = characters
-            .map(::CharacterItemAdapter) // creating item adapters from characters using the CharacterItemAdapter
-        // constructor reference
+        swipeRefreshView.setOnRefreshListener { presenter.onRefresh() } // pass events to the presenter using its methods
+        presenter.onViewCreated()
+    }
+
+    override fun show(items: List<MarvelCharacter>) {
+        val categoryItemAdapters = items.map(::CharacterItemAdapter)
         recyclerView.adapter = MainListAdapter(categoryItemAdapters)
+    }
+
+    override fun showError(error: Throwable) {
+        toast("Error: ${error.message}")
+        error.printStackTrace()
     }
 }
