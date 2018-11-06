b7c21f7ba1bc957e918b5f86dc5d9c4047bc0ffb
Raffaella <raffaella.tran@dunnhumby.com>
2018-11-06 11:44:46 +0100

    clean up code

--- app/src/test/java/com/example/rafaellat/marvelgallery/helpers/Example.kt
+++ app/src/test/java/com/example/rafaellat/marvelgallery/helpers/Example.kt
@@ -4,10 +4,13 @@ import com.example.rafaellat.marvelgallery.model.MarvelCharacter
 
 
 object Example {
-    val exampleCharacter = MarvelCharacter("ExampleName", "ExampleImageUrl")
+    val list = listOf<String>()
+    val exampleCharacter =
+        MarvelCharacter("ExampleName", "ExampleImageUrl", "ExampleDescription", list, list, list, list)
     val exampleCharacterList = listOf(
         exampleCharacter,
-        MarvelCharacter("Name1", "ImageUrl1"),
-        MarvelCharacter("Name2", "ImageUrl2")
+        MarvelCharacter("Name1", "ImageUrl1", "Description1", list, list, list, list),
+        MarvelCharacter("Name2", "ImageUrl2", "Description1", list, list, list, list)
     )
 }
+
